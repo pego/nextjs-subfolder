a2d9b40b517165bdd19a6a58557959b1b94dfcc7
Massimo Pegoraro <massimo.pegoraro@atoms.studio>
2022-08-10 13:08:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,24 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   //basePath: '/spring-summer-2023-fashion-show',
   trailingSlash: true,
-  rewrites() {
-    return {
-        beforeFiles: [
-            // if the host is `app.acme.com`,
-            // this rewrite will be applied
-            {
-                source: '/:path*',
-                has: [
-                    {
-                        type: 'host',
-                        value: 'nextjs-subfolder.vercel.app',
-                    },
-                ],
-                destination: 'https://nextjs-root.vercel.app/spring-summer-2023-fashion-show/:path*',
-            },
-        ]
-    }
-}
 }
 
 module.exports = nextConfig
