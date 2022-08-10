6f38acbbd29fbc63a4e93a986d8f6ea3e848bd2f
Massimo Pegoraro <massimo.pegoraro@atoms.studio>
2022-08-10 10:36:59 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //basePath: '/spring-summer-2023-fashion-show',
+  basePath: '/spring-summer-2023-fashion-show',
   //trailingSlash: true,
 }
 
